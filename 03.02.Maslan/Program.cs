3e08cfc9bdf061ba4b75cbefb3f2d76e31e63794
Toni Stoyanov <optimusz@abv.bg>
2016-04-25 14:14:01 +0300

    03.02.Maslan

---
+++ 03.02.Maslan/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Numerics;
+namespace _03._01.SaddyCoper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            int counter = 0;
+            while (number.Length > 1 && counter < 10)
+            {
+                BigInteger result = 1;
+                while (number.Length > 0)
+                {
+                    number = number.Substring(0, number.Length - 1);
+                    int sum = 0;
+                    for (int i = 1; i < number.Length; i += 2)
+                    {
+                        sum += (number[i] - '0');
+                    }
+
+                    if (sum != 0)
+                    {
+                        result *= sum;
+                    }
+                }
+                counter++;
+                number = result.ToString();
+            }
+            if (counter < 10)
+            {
+                Console.WriteLine(counter);
+            }
+            Console.WriteLine(number);
+
+
+
+        }
+    }
+}
