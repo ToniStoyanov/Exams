955e6a92bb76cd029cc95ac2d2316833f2aba082
Toni Stoyanov <optimusz@abv.bg>
2016-04-25 12:42:08 +0300

    01.01.Printing

---
+++ 01.01.Printing/Printing.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace _01.Printing
+{
+    class Printing
+    {
+        static void Main(string[] args)
+        {
+            double N = double.Parse(Console.ReadLine());
+            double S = double.Parse(Console.ReadLine());
+            double P = double.Parse(Console.ReadLine());
+
+            double sheetsOfPaper = N * S;
+            double reams = sheetsOfPaper / 500;
+
+            Console.WriteLine("{0:F2}",reams * P);
+        }
+    }
+}
