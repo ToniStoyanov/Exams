34ad3231280d422934180395f4adc619c78fcf35
Toni Stoyanov <optimusz@abv.bg>
2016-04-25 12:20:39 +0300

    01.02.ABC
    exam

---
+++ 01.02.ABC/ABC.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _01._02.ABC
+{
+    class ABC
+    {
+        static void Main(string[] args)
+        {
+            double A = double.Parse(Console.ReadLine());
+            double B = double.Parse(Console.ReadLine());
+            double C = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(Math.Max(A,Math.Max(B,C)));
+            Console.WriteLine(Math.Min(A,Math.Min(B,C)));
+            Console.WriteLine("{0:F3}",(A+B+C)/3);
+        }
+    }
+}
